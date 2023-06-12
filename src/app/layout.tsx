69338bd698469d9a1af48d25433e3a2994d2ad9b
AleksRG <aleksrdev@gmail.com>
2023-06-12 20:19:25 +0300

    Remove metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,6 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Get last 100 SOL transcactions by wallet",
-  description: "NEXT.js x Solscan Public API",
-};
-
 export default function RootLayout({
   children,
 }: {
